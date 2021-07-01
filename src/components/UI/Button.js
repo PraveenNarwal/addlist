91555bf55205bd780d5f1e99e16add2e58b84717
PraveenNarwal <praveennarwal5@gmail.com>
2021-07-01 18:31:04 +0530

    props and html  properties different

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -5,7 +5,7 @@ function Button(props) {
     return (
         <button className={classes.button}
             type={props.type || 'button'}
-            onClick={props.onclick}
+            onClick={props.onClick}
         >
             {props.children}
         </button>
